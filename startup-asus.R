38acdbc937ab6d5ffcb0abc34bfd7986d9257a3c
zeb-yoko <zebadiah.yoko@gmail.com>
2018-09-05 15:35:43 -0500

    -add startup file

---
+++ startup-asus.R
@@ -0,0 +1,13 @@
+library(car); library(dhglm); library(dplyr); library(effects);
+library(ggplot2); library(glmm); library(gridExtra); library(lme4)
+library(lmerTest);library(mcmc); library(plotly); library(reshape2); 
+library(rptR); library(corr)
+#setwd("C:/Users/zebadiah/NDSU Dropbox/Dropbox/Geum triflorum/Master_Datasheets")
+##ASUS
+setwd("C:/Users/zippy/Dropbox/Geum triflorum/Master_Datasheets")
+#set colors   #Prairie   #GL ALvar  #MB Alvar
+col.esa <- c("#18563E", "#82BE42", "#FFC423")
+setwd("C:/Users/zippy/OneDrive/NDSU/Rstudio-code")
+#csv loadin workaround
+colnames(VECTOR)[1] = "Column_Name"
+

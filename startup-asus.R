3d576a004158d15cb6e110271726bffb643a88fe
zeb-yoko <zebadiah.yoko@gmail.com>
2018-09-12 15:37:47 -0500

    -fixed corrr and streamlined tidyverse libraries

--- startup-asus.R
+++ startup-asus.R
@@ -1,10 +1,10 @@
 ########ADVANCED ECOLOGY VERSION#########
 ##Zebadiah.Yoko@gmail.com##
 
-library(car); library(dhglm); library(dplyr); library(effects);
-library(ggplot2); library(glmm); library(gridExtra); library(lme4)
+library(car); library(dhglm); library(effects);
+library(glmm); library(gridExtra); library(lme4)
 library(lmerTest);library(mcmc); library(plotly); library(reshape2); 
-library(rptR); library(corr)
+library(rptR); library(corrr); library(tidyverse)
 #setwd("C:/Users/zebadiah/NDSU Dropbox/Dropbox/Geum triflorum/Master_Datasheets")
 ##ASUS
 setwd("C:/Users/zippy/Dropbox/Geum triflorum/Master_Datasheets")
@@ -13,6 +13,4 @@ col.esa <- c("#18563E", "#82BE42", "#FFC423")
 setwd("C:/Users/zippy/OneDrive/NDSU/Rstudio-code")
 #csv loadin workaround
 colnames(VECTOR)[1] = "Column_Name"\
-
-#hi Jeremy
 
